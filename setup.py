c8136485cbf7c3a7fdfdfa87a8249b948f161de5
karizmattic876 <matthewkcwilliams@gmail.com>
2025-04-18 23:11:51 -0500

    setup.py: project description added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cred-switcher",
-    version="0.2",
+    version="0.2.1",
     packages=find_packages(),
     install_requires=[
         "pyfiglet"
@@ -15,4 +15,12 @@ setup(
 
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
+    description="A CLI tool to switch between Git and AWS credentials",
+    author="Karizmattic876",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
